a79d75a0f646855e638f2dfec720bc05a367c113
tapiwanashe004 <matizataps004@gmail.com>
2025-08-21 14:10:51 +0200

    Add files via upload

---
+++ Taps.py
@@ -0,0 +1,21 @@
+#PROGRAMING ASSIGNMENT 1#
+
+
+#In programing, variables refers to named storage locations that hold data which can change during program execution#
+user_name= "Tapiwanashe"  #Here username is a variable as it can change during program execution depending on the user
+                         # at that time#
+
+#Constants refers to named locations whose values remain fixed throughout the
+# program's execution. Python does not have built-in constants but people can use uppercase to indicate a constant#
+Pi= 3.14159 #Pi is a constant that stores the value 3.14159. #
+
+#The print function refers to the fundamental output operation that displays information to the console or another output device#
+print(user_name )
+print(Pi)
+
+
+#Below is a code with variables, constant and the print function#
+radius= 10
+Pi= 3.14159
+area= Pi*(radius**2)
+print("The area of the circle that Tapiwanashe calculated is:",area)
